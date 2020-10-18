4702f5fecbc593d1cdfc6d0a1a3c24bc7771f115
Derek Li <derek.j.h.li@gmail.com>
2020-10-18 16:14:41 +0800

    basic commit

---
+++ random.py
@@ -0,0 +1,13 @@
+import random
+r = random.randint(1, 100)
+while True :
+	num = eval(input("請猜一個數字: "))
+	if num == r :
+		print("終於猜對了")
+		break
+	elif num >= r :
+		print("猜錯了，你的數字比答案大")
+	elif num < r :
+		print("猜錯了，你的數字比答案小")
+	else :
+		print("僅能輸入數字型態")
