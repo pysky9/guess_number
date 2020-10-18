cde09bbb05cf257d2de1e79bdcdfb3a3fb59cfb9
Derek Li <derek.j.h.li@gmail.com>
2020-10-18 16:31:04 +0800

    third commit : guessing times

--- random.py
+++ random.py
@@ -1,8 +1,11 @@
 import random
 r = random.randint(1, 100)
+i = 0
 while True :
+	i += 1
 	num = eval(input("請猜一個數字: "))
 	if num == r :
+		print(f'你猜了{i}次')
 		print("終於猜對了")
 		break
 	elif num > r :
