5ba94e473c513de05d51cd918e54c91213b363af
Derek Li <derek.j.h.li@gmail.com>
2020-10-18 16:39:11 +0800

    4th commit : update

--- random.py
+++ random.py
@@ -5,10 +5,11 @@ while True :
 	i += 1
 	num = eval(input("請猜一個數字: "))
 	if num == r :
-		print(f'你猜了{i}次')
 		print("終於猜對了")
+		print(f'你猜了{i}次')
 		break
 	elif num > r :
 		print("猜錯了，你的數字比答案大")
 	elif num < r :
 		print("猜錯了，你的數字比答案小")
+	print(f'你猜了{i}次')
